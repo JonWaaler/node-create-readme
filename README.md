9a706c36007631484a1891d9e668e52d9d09d980
JonWaaler <jonwaaler@outlook.com>
2021-04-21 23:18:45 -0400

    massive changes over the last few days

--- README.md
+++ README.md
@@ -1,14 +1,41 @@
-# My Project
+# My projevct
 
-## Project Description <a name="project-desc"></a>
+## Project Description <a name='project-desc'></a>
 
-This is a project i made
+description
 
 ## Table of Contents
 
-- [Project Description](#project-desc)
-- [Installation](##installation)
+* [Project Description](#project-desc)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contribution Guidelines](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
-## Installation
 
-Go to github a clone the project
+## Installation <a name='installation'></a>
+
+installation
+
+## Usage <a name='usage'></a>
+
+usage of project
+
+## License <a name='license'></a>
+
+BSD 2-Clause
+
+## Contribution Guidelines <a name='contributing'></a>
+
+you can contribute to this project by doing some sorta bullshit
+
+## Tests <a name='tests'></a>
+
+you can perform a certain amount of tests just by using the command ""node index run tests"
+
+## Questions <a name='questions'></a>
+
+[My Github:jonwaaler](https://github.com/jonwaaler)
+ email: jonwaaler@gmail.com
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function init() {
       {
         type: "input",
         name: "install",
-        message: "How do other install this project?",
+        message: "How do others install this project?",
       },
       {
         type: "input",
@@ -39,22 +39,42 @@ function init() {
           "Attribution NonCommercial 4.0",
           "Attribution NoDerivates 4.0",
           "Attribution NonCommercialShareAlike 4.0",
-          new inquirer.Separator(),
           "Attribution NonCommercialNoDerivatives",
-          "Ecipse Public License 1.0",
+          new inquirer.Separator(),
           "GNU GPL v3",
           "GNU GPL v2",
           "GNU AGPL v3",
-          new inquirer.Separator(),
           "GNU LGPL v3",
           "GNU FDL v1.3",
+          new inquirer.Separator(),
+          "Ecipse Public License 1.0",
           "IBM",
           "ISC",
           "MIT",
-          new inquirer.Separator(),
           "Mozilla",
           "ODC BY",
         ],
+      },
+      {
+        type: "input",
+        name: "contribution",
+        message: "How can others contribute to this project?",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "How do you perform tests?",
+      },
+      {
+        type: "input",
+        name: "username",
+        message:
+          "What is you github user name? (ex: https://github.com/<username>)",
+      },
+      {
+        type: "input",
+        name: "contact",
+        message: "What is your email for contacting?",
       },
     ])
     .then((answers) => {
@@ -62,26 +82,39 @@ function init() {
 
       // Save collected answers
       ClearReadMe();
-      SaveFile("# " + Capitalize(answers.title));
+
+      // Savefile uses an async function meaning all text has to go at once.
       SaveFile(
-        "\n\n## Project Description <a name='project-desc'></a>\n\n" +
-          answers.description
-      );
-      SaveFile(
-        "\n\n## Table of Contents\n\n" +
+        "# " +
+          Capitalize(answers.title) +
+          "\n\n## Project Description <a name='project-desc'></a>\n\n" +
+          answers.description +
+          "\n\n## Table of Contents\n\n" +
           "* [Project Description](#project-desc)\n" +
           "* [Installation](#installation)\n" +
           "* [Usage](#usage)\n" +
           "* [License](#license)\n" +
           "* [Contribution Guidelines](#contributing)\n" +
-      );
-      SaveFile(
-        "\n\n## Installation <a name='installation'></a>\n\n" + answers.install
-      );
-      SaveFile("\n\n## Usage <a name='usage'></a>\n\n" + answers.usage);
-      SaveFile("\n\n## License <a name='license'></a>\n\n");
-      SaveFile(
-        "\n\n## Contribution Guidelines <a name='contributing'></a>\n\n"
+          "* [Tests](#tests)\n" +
+          "* [Questions](#questions)\n" +
+          "\n\n## Installation <a name='installation'></a>\n\n" +
+          answers.install +
+          "\n\n## Usage <a name='usage'></a>\n\n" +
+          answers.usage +
+          "\n\n## License <a name='license'></a>\n\n" +
+          answers.license +
+          "\n\n## Contribution Guidelines <a name='contributing'></a>\n\n" +
+          answers.contribution +
+          "\n\n## Tests <a name='tests'></a>\n\n" +
+          answers.tests +
+          "\n\n## Questions <a name='questions'></a>\n\n" +
+          "[My Github:" +
+          answers.username +
+          "](https://github.com/" +
+          answers.username +
+          ")" +
+          "\n email: " +
+          answers.contact
       );
     })
     .catch((error) => {
@@ -95,10 +128,10 @@ function init() {
     });
 }
 
+const fs = require("fs");
+
 // Runs the prompts
 init();
-
-const fs = require("fs");
 
 // Empty the read start with title
 ClearReadMe = () => {
@@ -122,6 +155,7 @@ SaveFile = (ans) => {
   }
 };
 
+// Capitalize title
 Capitalize = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
